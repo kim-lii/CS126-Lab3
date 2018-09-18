4a3fdc1a1fd8473d8a144836c03981281676f732
kim-lii <li.kim.0915@gmail.com>
2018-09-18 12:29:42 -0500

    Included user input, added comments

--- src/main.java
+++ src/main.java
@@ -6,8 +6,19 @@ import java.util.Scanner;
 public class main {
 
     public static void main(String[] args) {
-        String url = "http://erdani.com/tdpl/hamlet.txt";
-        System.out.println(urlToString(url));
+
+        //inputs user variables
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter a url: ");
+        String website = userInput.nextLine();
+        System.out.println("Input a search term: ");
+        String searchTerm = userInput.nextLine();
+        userInput.close();
+
+        //tests webcrawler
+        String tester = urlToString(website);
+        System.out.println(tester);
+        System.out.println("Found " + wordCount(tester, searchTerm) + " times");
     }
 
     /**
@@ -16,7 +27,7 @@ public class main {
      * @param url url to retrieve contents from
      * @return the contents from the url as a string, or an empty string on error
      */
-    public static String urlToString(final String url) {
+    private static String urlToString(final String url) {
         Scanner urlScanner;
         try {
             urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
@@ -26,6 +37,23 @@ public class main {
         String contents = urlScanner.useDelimiter("\\A").next();
         urlScanner.close();
         return contents;
+    }
+
+    /**
+     *
+     * @param contents string of which to parse through
+     * @param searchTerm term searched for
+     * @return the number of times the searchTerm was found
+     */
+    private static int wordCount(final String contents, final String searchTerm) {
+        int wordLength = searchTerm.length();
+        int countedTimes = 0;
+        for (int index = 1; index < (contents.length() - wordLength); index++) {
+            if (contents.substring(index, index + wordLength).equalsIgnoreCase(searchTerm)) {
+                countedTimes++;
+            }
+        }
+        return countedTimes;
     }
 
 }
